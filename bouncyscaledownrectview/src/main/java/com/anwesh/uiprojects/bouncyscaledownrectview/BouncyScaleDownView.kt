b251f064f2d39189840f6dc91d3e18691225fe68
Anwesh43 <anweshthecool0@gmail.com>
2020-01-12 12:15:28 +0530

    adding constants and libraries

--- bouncyscaledownrectview/src/main/java/com/anwesh/uiprojects/bouncyscaledownrectview/BouncyScaleDownView.kt
+++ bouncyscaledownrectview/src/main/java/com/anwesh/uiprojects/bouncyscaledownrectview/BouncyScaleDownView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.bouncyscaledownrectview
 /**
  * Created by anweshmishra on 12/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val bars : Int = 5
+val scGap : Float = 0.02f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
