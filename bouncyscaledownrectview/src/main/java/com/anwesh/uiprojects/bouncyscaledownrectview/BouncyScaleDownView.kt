0839a1f0e423f9b01059d5d26ee323cab7c8689d
Anwesh43 <anweshthecool0@gmail.com>
2020-01-12 12:31:26 +0530

    adding BouncyScaleDownView class

--- bouncyscaledownrectview/src/main/java/com/anwesh/uiprojects/bouncyscaledownrectview/BouncyScaleDownView.kt
+++ bouncyscaledownrectview/src/main/java/com/anwesh/uiprojects/bouncyscaledownrectview/BouncyScaleDownView.kt
@@ -50,3 +50,21 @@ fun Canvas.drawSBDNode(i : Int, scale : Float, paint : Paint) {
     drawBars(scale, w, paint)
     restore()
 }
+
+class BouncyScaleDownView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
