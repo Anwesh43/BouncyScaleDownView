9a53a056a140bebcae1c2d2caa3b210a66e7b8ba
Anwesh43 <anweshthecool0@gmail.com>
2020-01-12 12:43:40 +0530

    adding SBDNode class

--- bouncyscaledownrectview/src/main/java/com/anwesh/uiprojects/bouncyscaledownrectview/BouncyScaleDownView.kt
+++ bouncyscaledownrectview/src/main/java/com/anwesh/uiprojects/bouncyscaledownrectview/BouncyScaleDownView.kt
@@ -116,4 +116,46 @@ class BouncyScaleDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBDNode(var i : Int, val state : State = State()) {
+
+        private var next : SBDNode? = null
+        private var prev : SBDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SBDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBDNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBDNode {
+            var curr : SBDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
